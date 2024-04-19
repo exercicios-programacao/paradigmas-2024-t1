ccff4a1874040755dbcba21784df36ad1d603c5d
Vinicius Finger <fingervinicius@icloud.com>
2024-04-19 20:16:43 -0300

    push back and is empty methods

--- src/lista.c
+++ src/lista.c
@@ -24,12 +24,46 @@ void Lista_new(Lista* list, int data_type_size, void (*free_data)(void*)) {
 
 void Lista_pushFront(Lista* list, void* data) {
     NodeLista* new_node = (NodeLista*) malloc(sizeof(NodeLista));
+
     new_node->data = malloc(list->data_type_size);
+
     memcpy(new_node->data, data, list->data_type_size);
+
     new_node->next = list->head;
+
     list->head = new_node;
+
     list->size++;
 }
+
+void Lista_pushBack(Lista* list, void* data) {
+    NodeLista* new_node = (NodeLista*) malloc(sizeof(NodeLista));
+    
+    new_node->data = malloc(list->data_type_size);
+
+    memcpy(new_node->data, data, list->data_type_size);
+
+    new_node->next = NULL;
+
+    if (list->head == NULL) {
+        list->head = new_node;
+    } else {
+        NodeLista* current_node = list->head;
+        
+        while (current_node->next != NULL) {
+            current_node = current_node->next;
+        }
+
+        current_node->next = new_node;
+    }
+
+    list->size++;
+}
+
+Lista_isEmpty(Lista* list) {
+    return list->size == 0;
+}
+
 
 
 
--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
 
     int nums[] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++) {
-        Lista_pushFront(&intlist, &nums[i]);
+        Lista_pushBack(&intlist, &nums[i]);
     }
 
     NodeLista* currentNode = intlist.head;
