ed0b1744125bb608ac85be127ceefc761dadd849
Vaeep <diogobonofre@gmail.com>
2024-04-30 17:43:27 -0300

    Added cmp_int/double/string

--- src/main.c
+++ src/main.c
@@ -1,19 +1,35 @@
-/**
- * Este arquivo pode ser utilizado para criar um teste rápido
- * que permita depurar a função que está atualmente sendo
- * implementada de forma mais simples.
- *
- * Para compilar apenas esse programa, utilize `make main`.
- *
- * Você pode alterar como desejar esse programa, ele não será
- * avaliado pelos testes.
- */
-
-#include <stdio.h>
-
 #include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!\n");
-    return 0;
+int int_cmp(void *lhs, void *rhs);
+int double_cmp(void *lhs, void *rhs);
+int string_cmp(void *lhs, void *rhs);
+
+int main(int argc, char **argv) {
+  printf("Hello World!\n");
+  return 0;
+}
+
+int int_cmp(void *lhs, void *rhs) {
+  int *lhs_ptr = (int *)lhs;
+  int *rhs_ptr = (int *)rhs;
+  return (*lhs_ptr - *rhs_ptr);
+}
+
+int double_cmp(void *lhs, void *rhs) {
+  double *lhs_ptr = (double *)lhs;
+  double *rhs_ptr = (double *)rhs;
+  if ((*lhs_ptr - *rhs_ptr) > 0)
+    return 1;
+  if ((*lhs_ptr - *rhs_ptr) < 0)
+    return -1;
+  return 0;
+}
+
+int string_cmp(void *lhs, void *rhs) {
+  char *lhs_ptr = (char *)lhs;
+  char *rhs_ptr = (char *)rhs;
+  return strcmp(lhs_ptr, rhs_ptr);
 }
